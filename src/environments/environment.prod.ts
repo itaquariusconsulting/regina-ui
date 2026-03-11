0f62613517548513427acb14d53e04ba663a386d
itaqdeveloper02 <itaqdeveloper02@gmail.com>
2026-03-11 11:12:28 -0500

    commit environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrlAuth: 'https://marcaciongps.aquariusconsultores.com:8443/regina-billing-dev',
   apiUrlProcess: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
-  apiUrlUtils: "http://38.187.16.148:21678/sai-web-utils-dev/api/utils/",
+  apiUrlUtils: "https://marcaciongps.aquariusconsultores.com:8443/sai-web-utils-dev/api/utils/",
   apiUrlMaestros: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
-  apiUrlIA: 'http://localhost:6700',
-  apiUrlOcr: 'http://localhost:6701'
+  apiUrlIA: 'http://192.168.50.248:7001',
+  apiUrlOcr: 'http://192.168.50.248:7000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrlAuth: 'https://marcaciongps.aquariusconsultores.com:8443/regina-billing-dev',
   apiUrlProcess: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
-  apiUrlUtils: "http://38.187.16.148:21678/sai-web-utils-dev/api/utils/",
+  apiUrlUtils: "https://marcaciongps.aquariusconsultores.com:8443/sai-web-utils-dev/api/utils/",
   apiUrlMaestros: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
   apiUrlIA: 'http://192.168.50.248:7001',
   apiUrlOcr: 'http://192.168.50.248:7000'
