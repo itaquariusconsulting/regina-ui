362ddcd61ffea86ce94fc13e8367da133cd454a8
itaqdeveloper04 <itaqdeveloper04@gmail.com>
2026-03-19 12:41:15 -0500

    Feat: Implement PATCH User

--- src/app/services/reg-sec-user.service.ts
+++ src/app/services/reg-sec-user.service.ts
@@ -13,35 +13,30 @@ export class RegSecUserService {
 
   constructor(private http: HttpClient) { }
 
-
   token = sessionStorage.getItem('authToken');
   apiUrlProcess: string = environment.apiUrlProcess;
   apiUrlAuth: string = environment.apiUrlAuth;
 
   getRegSecUsers(wrapper: WrapperRequestUsuario): Observable<Response> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`,
-      'Content-Type': 'application/json'
-    });
     return this.http.get<Response>(`${this.apiUrlAuth}/api/usuario/listar/${wrapper.codEmpresa}/${wrapper.codSucursal}`, {
-      headers,
-      responseType: 'json'
+      headers: this.getHeaders()
     });
   }
 
   saveRegSecUser(usuario: RegSecUser): Observable<Response> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`,
-      'Content-Type': 'application/json'
-    });
     return this.http.post<Response>(`${this.apiUrlAuth}/api/usuario/guardar`, usuario, {
-      headers,
-      responseType: 'json'
+      headers: this.getHeaders(),
     });
   }
 
   getUserById(id: number): Observable<Response> {
     return this.http.get<Response>(`${this.apiUrlAuth}/api/usuario/${id}`, { headers: this.getHeaders() });
+  }
+
+  patchUser(user: RegSecUser): Observable<Response> {
+    return this.http.patch<Response>(`${this.apiUrlAuth}/api/usuario/actualizar-parcial`, user, {
+      headers: this.getHeaders()
+    });
   }
 
   private getHeaders(): HttpHeaders {
